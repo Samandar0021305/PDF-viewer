ac4cefd4b720da22b90b4f4bddba74a1e3814d6c
tsamandardev89 <tsamandardev89@gmail.com>
2025-10-31 15:10:47 +0500

    update

--- src/components/PdfViewer.vue
+++ src/components/PdfViewer.vue
@@ -1,6 +1,13 @@
 <template>
-  <PDF src="../../public/B2 HJ.pdf" />
+  <PDF :src="pdfUrl" />
 </template>
 <script setup>
 import PDF from "pdf-vue3";
+
+const props = defineProps({
+  pdfUrl: {
+    type: String,
+    required: true
+  }
+});
 </script>
