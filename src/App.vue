a3279b86539a528f183de94e3637d97ae5fac11f
tsamandardev89 <tsamandardev89@gmail.com>
2025-10-31 14:49:39 +0500

    update

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <PdfViewer />
+  </div>
+</template>
+
+<script>
+import PdfViewer from "./components/PdfViewer.vue";
+
+export default {
+  name: "App",
+  components: {
+    PdfViewer,
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100vh;
+}
+</style>
---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,7 @@
+<template>
+  <VPdfViewer src="../../public/B2 HJ.pdf" />
+</template>
+
+<script setup>
+import { VPdfViewer } from "@vue-pdf-viewer/viewer";
+</script>
